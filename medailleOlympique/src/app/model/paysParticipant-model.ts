823d799072bf56f32f8e15d4aa0060e9f5670094
Yae <letekomayao@gmail.com>
2024-03-29 22:33:11 +0100

    create component header

--- medailleOlympique/src/app/model/paysParticipant-model.ts
+++ medailleOlympique/src/app/model/paysParticipant-model.ts
@@ -1,6 +1,6 @@
 export class paysParticipant{
     constructor(
-        public  id: number,
+        public id: number,
         public nom:  string,
         public codeIso:  string,
         public comtinent: string,
