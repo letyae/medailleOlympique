7f47c41d493f9d96cbe4347e1e3c50fdd4e30fee
Yae <letekomayao@gmail.com>
2024-04-02 18:06:32 +0100

    creation de pays et pays-list component

---
+++ medailleOlympique/src/app/pays-list/pays-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pays-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './pays-list.component.html',
+  styleUrl: './pays-list.component.scss'
+})
+export class PaysListComponent {
+
+}
