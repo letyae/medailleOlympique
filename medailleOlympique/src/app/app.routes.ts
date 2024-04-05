618efa88ba986cadc4080fed9842904c0c70c8ed
Yae <letekomayao@gmail.com>
2024-04-05 17:34:13 +0200

    implementation du template de paysComponent #3 et creation de pays-single component

--- medailleOlympique/src/app/app.routes.ts
+++ medailleOlympique/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import {LandingpageComponent} from './landingpage/landingpage.component';
 import { PaysComponent } from './pays/pays.component';
+import { PaysListComponent } from './pays-list/pays-list.component';
 
 export const routes: Routes = [
+    {path:'allpays', component: PaysListComponent},
     {path:'pays', component: PaysComponent},
     {path:'', component: LandingpageComponent}
 ];
