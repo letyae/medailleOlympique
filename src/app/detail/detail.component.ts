ac5f03c7a3c14eec24b4c1b424851db7badf8fe4
Yae <letekomayao@gmail.com>
2024-04-16 17:51:39 +0200

    update of component

---
+++ src/app/detail/detail.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { MedailleComponent } from '../medaille/medaille.component';
+import { ActivatedRoute } from '@angular/router'; 
+import {medailleOlympique} from '../model/medailleOlympique-model'
+import {MedailleServices} from '../services/medailleService'
+
+@Component({
+  selector: 'app-detail',
+  standalone: true,
+  imports: [MedailleComponent],
+  templateUrl: './detail.component.html',
+  styleUrl: './detail.component.scss'
+})
+export class DetailComponent implements OnInit {
+  @Input() medaille!: medailleOlympique;
+
+  constructor(private medailleService: MedailleServices, private router: ActivatedRoute){}
+ 
+  ngOnInit(): void {
+    const paysId= +this.router.snapshot.params['id'];
+    this.medaille=this.medailleService.getMedailleById(paysId);
+    console.log("id="+paysId)
+  }
+}
