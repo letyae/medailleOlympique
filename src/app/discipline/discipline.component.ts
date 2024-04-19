c9ad6c7b9c524e99e33f6cba394b54b9afe310cb
Yae <letekomayao@gmail.com>
2024-04-19 21:41:40 +0200

    creation du component discipline

---
+++ src/app/discipline/discipline.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-discipline',
+  standalone: true,
+  imports: [],
+  templateUrl: './discipline.component.html',
+  styleUrl: './discipline.component.scss'
+})
+export class DisciplineComponent {
+
+}
