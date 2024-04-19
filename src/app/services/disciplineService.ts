6689d07187d9ff99dc4fa8414cd1acc47cc451a3
Yae <letekomayao@gmail.com>
2024-04-19 22:02:47 +0200

    implementation des services disciplineService.ts

---
+++ src/app/services/disciplineService.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { DisciplineSportive } from "../model/DisciplineSportive-model";
+import { error } from "console";
+
+@Injectable({
+    providedIn:'root'
+})
+export class DisciplineServices{
+    list_discipline =[
+        {
+          id: 1,
+          nom: "Boxe",
+          type: "",
+          popularite: "",
+          histoire: ""
+        },
+         {
+            id: 2,
+            nom: "Natation",
+            type: "",
+            popularite: "",
+            histoire: ""
+        },
+    ];
+
+    getAllDiscipline():DisciplineSportive[]
+    {
+        return this.list_discipline;
+    }
+
+    getDisciplineById(disciplineId: number): DisciplineSportive
+    { 
+        const  discipline = this.list_discipline.find(discipline => discipline.id === disciplineId);
+        if(discipline){ 
+        return discipline;
+        }else{
+        throw new Error("Discipline non trouvé");
+        }
+    } 
+
+}
